50732809329f4514b7a3bed4c01056a6c3980661
Filip Sjövall <filip.sjovall@gmail.com>
2023-03-05 19:09:22 +0100

    Ny assem funkar

---
+++ src/assemElem.jl
@@ -0,0 +1,119 @@
+using StaticArrays
+using LinearAlgebra
+ngp = 3
+
+ξ      = [2.0/3.0 1.0/3.0 1.0/3.0; 1.0/3.0 2.0/3.0 1.0/3.0; 1.0/3.0 1.0/3.0 2.0/3.0 ]
+w      = [1.0/3.0 1.0/3.0 1.0/3.0]
+index  = [1 2 3; 4 5 6; 7 8 9] 
+#index  = [1 4 7; 2 5 8; 3 6 9] 
+P₀     = [0.0 1.0 0.0; 0.0 0.0 1.0]
+#P₀     = [0.0 0.0; 1.0 0.0; 0.0 1.0]
+
+
+Np        = Matrix{Float64}(undef,3,6)
+#N = SMatrix{3,6,Float64}
+
+Np[:,1]   = ξ[1,:].*(2.0.*ξ[1,:] .- 1.0)
+Np[:,2]   = ξ[2,:].*(2.0.*ξ[2,:] .- 1.0)
+Np[:,3]   = ξ[3,:].*(2.0.*ξ[3,:] .- 1.0)
+Np[:,4]   = 4.0.*ξ[1,:].*ξ[2,:]
+Np[:,5]   = 4.0.*ξ[2,:].*ξ[3,:]
+Np[:,6]   = 4.0.*ξ[1,:].*ξ[3,:]
+ 
+N = SMatrix{3,6,Float64}(Np)
+
+SdNᵣ      = Matrix{Float64}(undef,6,9)
+#dNᵣ      = @SMatrix zeros(6,9)
+SdNᵣ[:,1] = [4.0*ξ[1,1]-1.0 0.0 0.0 4.0*ξ[1,2] 0.0 4.0*ξ[1,3]]
+SdNᵣ[:,2] = [0.0 4.0*ξ[1,2]-1.0 0.0 4.0*ξ[1,1] 4.0*ξ[1,3] 0.0]
+SdNᵣ[:,3] = [0.0 0.0 4.0*ξ[1,3]-1.0 0.0 4.0*ξ[1,2] 4.0*ξ[1,1]]
+
+SdNᵣ[:,4] = [4.0*ξ[2,1]-1.0 0.0 0.0 4.0*ξ[2,2] 0.0 4.0*ξ[2,3]]
+SdNᵣ[:,5] = [0.0 4.0*ξ[2,2]-1.0 0.0 4.0*ξ[2,1] 4.0*ξ[2,3] 0.0]
+SdNᵣ[:,6] = [0.0 0.0 4.0*ξ[2,3]-1.0 0.0 4.0*ξ[2,2] 4.0*ξ[2,1]]
+
+SdNᵣ[:,7] = [4.0*ξ[3,1]-1.0 0.0 0.0 4.0*ξ[3,2] 0.0 4.0*ξ[3,3]]
+SdNᵣ[:,8] = [0.0 4.0*ξ[3,2]-1.0 0.0 4.0*ξ[3,1] 4.0*ξ[3,3] 0.0]
+SdNᵣ[:,9] = [0.0 0.0 4.0*ξ[3,3]-1.0 0.0 4.0*ξ[3,2] 4.0*ξ[3,1]]
+dNᵣ = SMatrix{6,9,Float64}(SdNᵣ)
+
+Jᵀ      = zeros(3,3)
+Jᵀ[:,1] = [1.0 1.0 1.0]
+Stress  = zeros(2,2)
+Bₗ₀      = zeros(3,12)
+H₀      = zeros(4,12)
+A       = zeros(3,4)
+B₀      = zeros(3,12)
+R       = zeros(4,4)
+eff     = zeros(2,2)
+
+
+
+S = zeros(2,2)
+
+
+function assemGP(coord,ed,gp,mp,t)
+    
+    @inbounds Jᵀ[:,2:3]     = transpose(dNᵣ[:,index[gp,:]]) * coord
+    J⁻                      = inv(Jᵀ)
+    detJ                    = det(Jᵀ)
+    dNₓ                     = P₀ * J⁻ * transpose(dNᵣ[:,index[gp,:]])
+
+    # Gradient matrices  
+    @inbounds H₀[1,1:2:11]  = dNₓ[1,:]
+    @inbounds H₀[2,1:2:11]  = dNₓ[2,:]
+    @inbounds H₀[3,2:2:12]  = dNₓ[1,:]
+    @inbounds H₀[4,2:2:12]  = dNₓ[2,:]
+
+    @inbounds Bₗ₀[1,1:2:11] = dNₓ[1,:]
+    @inbounds Bₗ₀[2,2:2:12] = dNₓ[2,:]  
+    @inbounds Bₗ₀[3,1:2:11] = dNₓ[2,:]
+    @inbounds Bₗ₀[3,2:2:12] = dNₓ[1,:]
+
+    A_temp = H₀*ed
+    @inbounds A[1,:]        = [A_temp[1] 0.0 A_temp[3] 0.0]
+    @inbounds A[2,:]        = [0.0 A_temp[2] 0.0 A_temp[4]]
+    @inbounds A[3,:]        = [A_temp[2] A_temp[1] A_temp[4] A_temp[3]]
+
+    B₀                      = Bₗ₀ + A*H₀
+
+
+    
+    # Deformation gradient
+    
+    eff[1,1] = A_temp[1] + 1.0
+    eff[1,2] = A_temp[2]
+    eff[2,1] = A_temp[3]
+    eff[2,2] = A_temp[4] + 1.0
+
+    ef = [eff[1,1] eff[1,2] eff[2,1] eff[2,2]]
+
+    # Stress and material tangent
+    es = neohooke1(ef,mp)
+    D  = dneohooke1(ef,mp)
+
+
+    S                       = [es[1]; es[2]; es[4]]
+
+    @inbounds Stress[1,:]   = [S[1] S[3]]
+    @inbounds Stress[2,:]   = [S[3] S[2]]
+    
+    @inbounds R[1:2,1:2]    = Stress
+    @inbounds R[3:4,3:4]    = Stress
+    
+    fₑ            = transpose(B₀)*S*detJ*t*w[gp]/2
+    kₑ            = (transpose(B₀)*D*B₀ + transpose(H₀)*R*H₀)*detJ*t*w[gp]/2 
+    
+    return kₑ, fₑ
+end
+
+function assemElem(coord,ed,mp,t)
+    kₑ = zeros(12,12)
+    fₑ = zeros(12)
+    for gp ∈ 1 : 3
+        ke, fe= assemGP(coord,ed,gp,mp,t)
+        kₑ += ke
+        fₑ += fe
+    end
+    return kₑ, fₑ
+end
--- src/run.jl
+++ src/run.jl
@@ -13,10 +13,12 @@ function load_files()
     include("element_routines.jl")
 
     include("fem.jl")
+
+    include("assemElem.jl")
 end
 
 load_files()
-filename = "mesh_fine_fine.txt"
+filename = "mesh.txt"
 
 coord, enod, edof = readAscii(filename);
 
@@ -42,12 +44,15 @@ function solver()
     
     # För rätt format kan z-koordinat tas bort i notepad++ med specialsök: ", 0\n" - replace
     #grid = get_ferrite_grid("data/mesh_fine_fine.inp")
-    grid = get_ferrite_grid("data/mesh_fine_fine.inp")
+    grid = get_ferrite_grid("data/mesh.inp")
     dh = DofHandler(grid)
     add!(dh, :u, 2)
     close!(dh)
-    K = create_sparsity_pattern(dh)
+    K  = create_sparsity_pattern(dh)
 
+    K2 = create_sparsity_pattern(dh)
+    F2 = zeros(ndof)
+    
     Fᵢₙₜ     = zeros(ndof)
     Fₑₓₜ     = zeros(ndof)
     a        = zeros(ndof)
@@ -63,14 +68,14 @@ function solver()
     fₑ       = zeros(12)
 
 
-    bcdof,bcval = setBC(0.005,dh)
+    bcdof,bcval = setBC(0.1,dh)
     pdofs       = bcdof
     fdofs       = setdiff(1:ndof,pdofs)
     # bcval    = zeros(2)
     # bcval    = [0.0; 0.005]
     # bcdof    = Vector{Int64}(undef,2)
     #bcdof    = [1;7] - fine
-    # bcdof = [1;4]
+    
 
     #bcval    = zeros(6)
     #bcval    = [0.0; 0.0; 0.05; 0.05; 0.0; 0.0]
@@ -93,11 +98,12 @@ function solver()
         while (iter < imax && residual > TOL ) || iter < 2
 
             iter += 1
-            assembler = start_assemble(K,Fᵢₙₜ)
+            
             a += Δa
             #@time for ie ∈ 1 : nelm
             ie = 0
-            for cell in CellIterator(dh)
+            assembler = start_assemble(K,Fᵢₙₜ)
+            @time for cell in CellIterator(dh)
                 ie +=1                
                 fill!(kₑ, 0.0)
                 fill!(fₑ, 0.0)
@@ -122,56 +128,30 @@ function solver()
                 c2tl6_e!(kₑ, coord[enod[ie][2:7],:], t, D,  a[cell_dofs], es)
                 c2tl6_f!(fₑ, coord[enod[ie][2:7],:], t, es, a[cell_dofs])
 
-                
                 assemble!(assembler, cell_dofs, kₑ,fₑ)
-
-                # println(kₑ)
-                #@inbounds Fᵢₙₜ[cell_dofs] += fₑ            
+       
             end
 
-            # println("F_int ",Fᵢₙₜ[fdofs])
-            # println("F2 ", F2[fdofs])
-            
+
+            # Funktion "assemGlobal"
+            assembler2 = start_assemble(K2,F2)
+            ie = 0
+            @time for cell in CellIterator(dh)
+                ie += 1
+                fill!(kₑ, 0.0)
+                fill!(fₑ, 0.0)
+                cell_dofs= celldofs(cell)
+                kₑ, fₑ = assemElem(coord[enod[ie][2:7],:],a[cell_dofs],mp,t)
+                assemble!(assembler2, cell_dofs, kₑ, fₑ)
+            end
+
+            println(norm(K) - norm(K2))
         
             res = Fᵢₙₜ - Fₑₓₜ
             # prob = LinearProblem(K[fdofs,fdofs], -res[fdofs] - K[fdofs,pdofs]*bcval)
             # Δa[fdofs] = solve(prob, MKLPardisoIterate()).u
+            
             solveq!(Δa, K, -Fᵢₙₜ, bcdof, bcval)
-
-            #IterativeSolvers.cg!(Δa[fdofs], K[fdofs,fdofs], (-res[fdofs] - K[fdofs,pdofs]*bcval); maxiter=1000)
-            #Δa[pdofs] = bcval
-            # println("HL ", K2[fdofs,pdofs]*bcval)
-            # println("HL ", K[fdofs,pdofs]*bcval)
-            # if iter == 2
-            #     println("a ", a)
-            #     println("Δa ", Δa)
-            #     println("bcdof ", bcdof)
-            #     println("pdof ", pdofs)
-            #     iter = imax
-            #     spy(K)
-            # end
-            # a += Δa
-            # Fᵢₙₜ  = Fᵢₙₜ .*0
-
-            # for ie ∈ 1:nelm
-            #     fₑ       = fₑ.*0
-
-            #     c2tl6_d!( F, a[edof[ie,:]], coord[enod[ie][2:7],:] )
-            #     for gp = 1:3
-            #         ef[:,gp] = [F[gp,1,1] F[gp,1,2] F[gp,2,1] F[gp,2,2]]
-            #     end
-                
-            #     ## S - neohooke
-            #     neohookeS!(Sgp,ef,mp)
-            #     for gp = 1:3 
-            #         es[1,gp] = Sgp[1,gp] # Sₑ som namn istället?
-            #         es[2,gp] = Sgp[2,gp]
-            #         es[3,gp] = Sgp[4,gp]
-            #     end 
-
-            #     c2tl6_f!(fₑ, coord[enod[ie][2:7],:], t, es, a[edof[ie,:]])
-            #     Fᵢₙₜ[edof[ie,:]]           += fₑ            
-            # end
 
             bcval      = 0*bcval
             res        = Fᵢₙₜ - Fₑₓₜ
