ae26766cdd58a84e2a469b2966434c9eaaed9d63
Filip Sjövall <filip.sjovall@gmail.com>
2023-06-16 16:30:11 +0200

    Känsligheter stämmer igen för box till box, verkar bero mkt på nätet. Nästa steg är att få igång optimeringen

---
+++ src/Linear/OptimizationLinC.jl
@@ -0,0 +1,323 @@
+using Mortar2D, ForwardDiff
+using Ferrite, FerriteGmsh, FerriteMeshParser
+using LinearSolve, SparseArrays # LinearSolvePardiso
+using IterativeSolvers, IncompleteLU    # AlgebraicMultigrid
+using SparseDiffTools, Symbolics
+
+include("..//mesh_reader.jl")
+include("initLin.jl") # initieras massa skit
+include("Contact//contact_help.jl")
+include("assemLin.jl")
+include("assemElemLin.jl")
+include("..//material.jl")
+include("..//fem.jl")
+#include("runLinContact.jl")
+include("run_linear.jl")
+include("sensitivitiesLin.jl")
+
+include("..//mma.jl")
+
+#include("initLin.jl")
+
+
+# Create two grids
+grid1 = createBoxMeshRev("box_1", 0.0, 0.0, 1.0, 1.0, 0.15)
+grid2 = createBoxMeshRev("box_2", 0.0, 0.99, 1.0, 1.0, 0.1)
+
+# Merge into one grid
+grid_tot = merge_grids(grid1, grid2; tol=1e-6)
+
+# Create dofhandler with displacement field u
+dh = DofHandler(grid_tot)
+
+
+
+add!(dh, :u, 2)
+close!(dh)
+
+
+# Extract CALFEM-style matrices
+coord, enod = getTopology(dh)
+register = index_nod_to_grid(dh, coord)
+
+# --------------------------------------------------------------------------- #
+# These are useful if Shape functions/gradients defined by Ferrite are needed #
+# --------------------------------------------------------------------------- #
+ip = Lagrange{2,RefTetrahedron,1}();
+qr = QuadratureRule{2,RefTetrahedron}(1);
+qr_face = QuadratureRule{1,RefTetrahedron}(1);
+cv = CellVectorValues(qr, ip);
+fv = FaceVectorValues(qr_face, ip);
+
+# ------------------ #
+# Create master sets #
+# ------------------ #
+addfaceset!(dh.grid, "Γ_slave", x -> x[2] ≈ 1.0)
+Γs = getfaceset(dh.grid, "Γ_slave")
+
+addnodeset!(dh.grid, "nₛ", x -> x[2] ≈ 1.0)
+nₛ = getnodeset(dh.grid, "nₛ")
+
+# ----------------- #
+# Create slave sets #
+# ----------------- #
+addfaceset!(dh.grid, "Γ_master", x -> x[2] ≈ 0.99)
+Γm = getfaceset(dh.grid, "Γ_master")
+
+addnodeset!(dh.grid, "nₘ", x -> x[2] ≈ 0.99)
+nₘ = getnodeset(dh.grid, "nₘ")
+
+# Extract all nbr nodes and dofs
+contact_dofs = getContactDofs(nₛ, nₘ)
+contact_nods = getContactNods(nₛ, nₘ)
+order = Dict{Int64,Int64}()
+for (i, nod) ∈ enumerate(contact_nods)
+    push!(order, nod => i)
+end
+freec_dofs = setdiff(1:dh.ndofs.x, contact_dofs)
+
+# Define top nodeset for displacement controlled loading
+addnodeset!(dh.grid, "Γ_top", x -> x[2] ≈ 1.49)
+Γ_top = getnodeset(dh.grid, "Γ_top")
+
+
+# Define bottom nodeset subject to  u(X) = 0 ∀ X ∈ Γ_bot
+addnodeset!(dh.grid, "Γ_bot", x -> x[2] ≈ 0.0)
+Γ_bot = getnodeset(dh.grid, "Γ_bot")
+
+
+# Final preparations for contact
+register = getNodeDofs(dh)
+X = getX(dh)
+coord = getCoordfromX(X)
+
+# Init fictious
+
+coord₀ = deepcopy(coord)
+Γ_robin = union(
+    getfaceset(dh.grid, "Γ_slave"),
+    getfaceset(dh.grid, "Γ_master")
+)
+n_robin = union(
+    getnodeset(dh.grid, "nₛ"),
+    getnodeset(dh.grid, "nₘ")
+)
+
+#for inod in nodx
+#   append!(free_d,register[inod,2]*2-1)
+#end
+free_d = []
+for jnod in n_robin
+    append!(free_d, register[jnod, 2] )
+end
+
+locked_d = setdiff(1:dh.ndofs.x,free_d)
+
+# boundary conditions for contact analysis
+bcdof_top_o, _ = setBCXY(-0.01, dh, Γ_top)
+bcdof_bot_o, _ = setBCXY(0.0, dh, Γ_bot)
+bcdof_o = [bcdof_top_o; bcdof_bot_o]
+
+
+ϵᵢⱼₖ = sortperm(bcdof_o)
+bcdof_o = bcdof_o[ϵᵢⱼₖ]
+bcval_o = bcdof_o .* 0.0
+
+# - For Linear solver..
+pdofs = bcdof_o
+fdofs = setdiff(1:dh.ndofs.x, pdofs)
+
+# Initialize tangents
+global K = create_sparsity_pattern(dh)
+global Kψ = create_sparsity_pattern(dh)
+global a = zeros(dh.ndofs.x)
+global Ψ = zeros(dh.ndofs.x)
+global Fᵢₙₜ = zeros(dh.ndofs.x)
+global rc = zeros(dh.ndofs.x)
+global Fₑₓₜ = zeros(dh.ndofs.x)
+global a = zeros(dh.ndofs.x)
+global Δa = zeros(dh.ndofs.x)
+global res = zeros(dh.ndofs.x)
+dh0 = deepcopy(dh)
+d   = zeros(size(a))
+d .= 0.0
+mp = [210 0.3] # [E ν]
+dFₑₓₜ_dx = similar(K)
+∂rᵤ_∂x = similar(K)
+dr_dd = similar(K)
+∂rψ_∂d = similar(K)
+λᵤ = similar(a)
+λψ = similar(a)
+include("initOptLin.jl")
+
+
+function Optimize(dh)
+
+    # Flytta allt nedan till init_opt?
+        dh0 = deepcopy(dh)
+        λψ   = similar(a)
+        λᵤ   = similar(a)
+        λᵥₒₗ = similar(a)
+        Vₘₐₓ = 2.0
+        global ε    = 20
+        l    = similar(a)
+        l   .= 0.5
+        tol  = 1e-6
+        OptIter = 0
+        coord₀  = coord
+        global g_hist = zeros(200)
+        global v_hist = zeros(200)
+    #
+    while kktnorm > tol || OptIter < 3 #&& OptIter < 50
+
+        # # # # # # # # # # # # # #
+        #       Definitions       #
+        # # # # # # # # # # # # # #
+            global d
+            global Ψ
+            global a
+            global Fₑₓₜ
+            global K
+            global Kψ
+            global ∂rᵤ_∂x
+            global dr_dd
+            global ∂rψ_∂d
+            global mp
+            global mp₀
+            global t
+            global m
+            global n
+            global epsimin
+            global xval
+            global xold1
+            global xold2
+            global xmin
+            global xmax
+            global low#A() = A(Float64[],[]).
+            global C
+            global d2
+            global a0
+            global outeriter
+            global am
+            global kkttol
+            global changetol
+            global kktnorm
+            global outit
+            global change
+            global λ
+            global g_ini
+            global pdofs = bcdof
+            global fdofs = setdiff(1:length(a), pdofs)
+            global locked_d = setdiff(1:length(a),free_d)
+            global low
+            global upp
+        # # # # # # # # # # # # # #
+        OptIter += 1
+
+        # # # # # # # # # # # # # #
+        # Fictitious equillibrium #
+        # # # # # # # # # # # # # #
+        coord₀ = getCoord(getX(dh0), dh0) # x₀
+        Ψ, _, Kψ, _, λ = fictitious_solver_C(d, dh0, coord₀)
+
+        # # # # # #
+        # Filter  #
+        # # # # # #
+        dh    = deepcopy(dh0)
+        updateCoords!(dh, Ψ) # x₀ + Ψ = x
+        coord = getCoord(getX(dh), dh)
+
+        # # # # # # # # #
+        # Equillibrium  #
+        # # # # # # # # #
+        a, _, Fₑₓₜ, Fᵢₙₜ, K, traction = solver_C(dh, coord)
+
+        # # # # # # #
+        # Objective #
+        # # # # # # #
+        #g = -a[pdofs]' * Fᵢₙₜ[pdofs]
+        #∂g_∂x[fdofs] = -a[pdofs]' * ∂rᵤ_∂x[pdofs, fdofs]
+        #∂g_∂u[fdofs] = -a[pdofs]' * K[pdofs, fdofs]
+        p = 4
+        X_ordered = getXfromCoord(coord)
+        g         = contact_pnorm(X_ordered, a, ε, p)
+        ∂g_∂x     = ForwardDiff.gradient(x -> contact_pnorm_ordered(x, a, ε, p),X_ordered)
+        ∂g_∂u     = ForwardDiff.gradient(u -> contact_pnorm(X_ordered, u, ε, p), a)
+
+        # # # # # # # # #
+        # Sensitivities #
+        # # # # # # # # #
+        ∂rᵤ_∂x       = drᵤ_dx_c(∂rᵤ_∂x, dh, mp, t, a, coord, enod, ε)
+        dr_dd        = drψ(dr_dd, dh0, Ψ, λ, d, Γ_robin, coord₀)
+
+        # # # # # # #
+        # Adjoints  #
+        # # # # # # #
+        solveq!(λᵤ, K', ∂g_∂u, bcdof_o, bcval_o)
+        solveq!(λψ, Kψ', ∂g_∂x - ∂rᵤ_∂x' * λᵤ, bcdof_o, bcval_o)
+
+        # # # # # # # # # # #
+        # Full sensitivity  #
+        # # # # # # # # # # #
+        ∂g_∂d = (-transpose(λψ) * dr_dd)'
+        #∂g_∂d[locked_d] .= 0.0 # fulfix?
+
+        # # # # # # # # # # #
+        # Volume constraint #
+        # # # # # # # # # # #
+        g₁    = volume(dh,coord,enod) / Vₘₐₓ - 1
+        ∂Ω_∂x = volume_sens(dh,coord)
+        solveq!(λᵥₒₗ, Kψ, ∂Ω_∂x, bcdof, bcval.*0);
+        ∂Ω∂d  = -transpose(λᵥₒₗ)*dr_dd ./ Vₘₐₓ;
+        ∂Ω∂d[locked_d] .= 0.0
+
+        # # # # #
+        # M M A #
+        # # # # #
+        X, ymma, zmma, lam, xsi, eta, mu, zet, S, low, upp = mmasub(m, n, OptIter, d, xmin, xmax, xold1, xold2, 10 * g, 10 * ∂g_∂d, g₁, ∂Ω∂d', low, upp, a0, am, C, d2)
+        xold2 = xold1
+        xold1 = d
+        d = X
+        change = norm(d .- xold1)
+
+        # # # # # # # # # #
+        # Postprocessing  #
+        # # # # # # # # # #
+        global v_hist[OptIter] = g₁
+        global g_hist[OptIter] = g
+
+        #The residual vector of the KKT conditions is calculated:
+        #residu,kktnorm,residumax = kktcheck(m,n,X,ymma,zmma,lam,xsi,eta,mu,zet,S, xmin,xmax,∂g_∂d,[0.0],zeros(size(d)),a0,a,C,d2);
+        kktnorm = change
+        println("Iter: ", OptIter, " Norm of change: ", kktnorm, " Objective: ", g)
+        if mod(OptIter,1) == 0
+            coord = getCoord(getX(dh0), dh0)
+            postprocess_opt(Ψ, dh0, "results/ShapeC" * string(OptIter))
+            coord = getCoord(getX(dh), dh)
+            postprocess_opt(a, dh, "results/DeformationC" * string(OptIter))
+        end
+        println("Objective: ", g_hist, " Constraint: ", g₁)
+        if OptIter == 50
+            break
+        end
+    end
+
+    # # # # # # # # #
+    # Plot history  #
+    # # # # # # # # #
+    fig = Figure()
+    ax1, l1 = lines(fig[1, 1], 1..OptIter, g_hist[1:OptIter], color = :red)
+    ax2, l2 = lines(fig[2, 1], 1..OptIter, v_hist[1:OptIter], color = :blue)
+    Legend(fig[1:2, 2], [l1, l2], ["Objective", "Constraint"])
+    fig
+    return g_hist, v_hist, OptIter
+end
+
+
+#using Makie
+#OptIter=37
+#fig = Figure()
+#ax1, l1 = lines(fig[1, 1], 1..OptIter, g_hist[1:OptIter], color = :red)
+#ax2, l2 = lines(fig[2, 1], 1..OptIter, v_hist[1:OptIter], color = :blue)
+#Legend(fig[1:2, 2], [l1, l2], ["Objective", "Constraint"])
+#fig
